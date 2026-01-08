27e739fcb2623c6ecad173b32fb3ebee0c9c3fe3
Abhiroop Santra <abhiroop.santra@gmail.com>
2026-01-08 15:32:51 +0400

    feat(frontend): :sparkles: implement event booking UI enhancements
    * Add `Event` and `User` models with required properties
    * Create `EventCard.razor` for event display
    * Enhance `TopEvents` to dynamically render multiple `EventCard` components
    * Revamp `NavMenu` with Bootstrap stylings and event search bar
    * Update `MainLayout` to include navigation and structured container
    * Update homepage to display top events
    * Integrate Bootstrap Icons for enhanced UI elements
    
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

--- EventBookings.Frontend/Models/Event.cs
+++ EventBookings.Frontend/Models/Event.cs
@@ -1,6 +1,21 @@
-﻿namespace EventBookings.Frontend.Models
+﻿namespace EventBookings.Frontend.Models;
+
+public class Event
 {
-    public class Event
-    {
-    }
+    public Guid Id { get; set; }
+    public string EventName { get; set; } = null!;
+    public string EventType { get; set; }
+    public string EventLocation { get; set; }
+    public string EventCountry { get; set; }
+    public DateTime EventDate { get; set; }
+    public int EventStartHour { get; set; }
+    public int EventEndHour { get; set; }
+    public int TotalSeatingCapacity { get; set; }
+    public int SeatsBooked { get; set; }
+    public decimal TicketPriceInUsd { get; set; }
+    public string CreatedBy { get; set; } = null!;
+    public DateTime CreatedDate { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public Guid ArtistId { get; set; }
 }
--- EventBookings.Frontend/Models/User.cs
+++ EventBookings.Frontend/Models/User.cs
@@ -1,6 +1,13 @@
-﻿namespace EventBookings.Frontend.Models
+﻿namespace EventBookings.Frontend.Models;
+
+public class User
 {
-    public class User
-    {
-    }
+    public Guid Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string MobileNumber { get; set; }
+    public string FullName { get; set; }
+    public string ResidenceCountryCode { get; set; }
+    public string TimeZone { get; set; }
+    public List<Event> FavoriteEvents { get; set; }
+    public List<Event> BookedEvents { get; set; }
 }
