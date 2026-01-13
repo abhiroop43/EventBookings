b4d1e3808eca016524aa0399b3ce4e43399477a8
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2026-01-13 16:18:18 +0400

    feat(building-blocks): :sparkles: add foundational building blocks for CQRS and exception handling
    * Introduced `ICommand`, `IQuery`, `ICommandHandler`, and `IQueryHandler` interfaces to support CQRS architecture.
    * Added custom exceptions: `NotFoundException`, `BadRequestException`, and `InternalServerException` for structured error handling.
    * Developed `LoggingBehavior` and `ValidationBehavior` pipeline behaviors for MediatR to enhance request processing.
    * Implemented `CustomExceptionHandler` to provide consistent exception responses in API.
    * Updated `BuildingBlocks.csproj` with package dependencies for FluentValidation, Mapster, MediatR, and Feature Management.
    
    Establishes a scalable foundation for future feature development.
    
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse> where TResponse : notnull
+{
+    private const int SlaThresholdInSeconds = 3;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        logger.LogInformation("[START] Handle Request={Request} - Response={Response} - RequestData={RequestData}",
+            typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        var response = await next(cancellationToken);
+
+        timer.Stop();
+
+        var timeTaken = timer.Elapsed;
+
+        if (timeTaken.Seconds > SlaThresholdInSeconds)
+            logger.LogWarning("[PERFORMANCE] The request {Request} took {TimeTaken} seconds", typeof(TRequest).Name,
+                timeTaken.Seconds);
+
+        logger.LogInformation("[END] handled {Request} with {Response}", typeof(TRequest).Name, typeof(TResponse).Name);
+
+        return response;
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Exceptions.Handler;
+
+public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger, IWebHostEnvironment environment)
+    : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        logger.LogError("Exception: {0} Occurred On: {1}", exception.Message, DateTime.UtcNow);
+
+        (string Detail, string Title, int StatusCode) detail = exception switch
+        {
+            InternalServerException => (
+                environment.IsDevelopment()
+                    ? exception.Message
+                    : "An error has occurred. Please contact the Administrator.", exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError),
+            ValidationException => (exception.Message, exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest),
+            BadRequestException => (exception.Message, exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest),
+            NotFoundException => (exception.Message, exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status404NotFound),
+            _ => (
+                environment.IsDevelopment()
+                    ? exception.Message
+                    : "An error has occurred. Please contact the Administrator.", exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError)
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = detail.Title,
+            Detail = detail.Detail,
+            Status = detail.StatusCode,
+            Instance = httpContext.Request.Path
+        };
+
+        problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
+
+        if (exception is ValidationException validationException)
+            problemDetails.Extensions.Add("errors", validationException.Errors);
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+        return true;
+    }
+}
